ef4d450fb29098a667b82ba4a0f139cacd9e7398
Layljan <ljdumayas@gmail.com>
2024-07-01 23:33:21 +0800

    Set up vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>Hello calculator</div>
+    </div>
+  );
+}
+
+export default App;
