8112e78aa774c412e2b90a2fd853b05fa39139e7
Layljan <ljdumayas@gmail.com>
2024-07-04 18:05:44 +0800

    Calculator design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import back from "./assets/back.svg";
 import "./App.css";
 
 function App() {
@@ -9,13 +10,65 @@ function App() {
       <body>
         <div className="calculator">
           <div className="num">
+            <button>7</button>
+          </div>
+          <div className="num">
+            <button>8</button>
+          </div>
+          <div className="num">
+            <button>9</button>
+          </div>
+          <div className="num">
+            <button>-</button>
+          </div>
+          <div className="num">
+            <button>
+              <img src={back} alt="" />
+            </button>
+          </div>
+          <div className="num">
+            <button>4</button>
+          </div>
+          <div className="num">
+            <button>5</button>
+          </div>
+          <div className="num">
+            <button>6</button>
+          </div>
+          <div className="num">
+            <button>X</button>
+          </div>
+          <div className="num">
+            <button>/</button>
+          </div>
+          <div className="num">
             <button>1</button>
           </div>
-          <div className="num">2</div>
-          <div className="num">3</div>
-          <div className="num">1</div>
-          <div className="num">2</div>
-          <div className="num">3</div>
+          <div className="num">
+            <button>2</button>
+          </div>
+          <div className="num">
+            <button>3</button>
+          </div>
+          <div className="num size-2">
+            <button>+</button>
+          </div>
+          <div className="num">
+            <button>-</button>
+          </div>
+          <div className="num">
+            <button>0</button>
+          </div>
+
+          <div className="num">
+            <button>.</button>
+          </div>
+          <div className="num">
+            <button>=</button>
+          </div>
+          <div className="num">
+            <button>C</button>
+          </div>
         </div>
       </body>
     </div>
